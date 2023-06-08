255b9694ae56442fb98d1ce7b0d95a1f36e5f146
newsoft111 <ydj0617@gmail.com>
2023-06-08 16:27:30 +0900

    Update crawler_get_url.py

--- crawler/crawler_get_url.py
+++ crawler/crawler_get_url.py
@@ -24,54 +24,59 @@ class DanakkaCrawler:
 	def sunsang24(self):
 		self.driver.get("https://www.google.com/search?q=")
 
-		url = "http://127.0.0.1:8000/fishing/api/site_url_none_data/"
+		self.danakka_url = "http://newsoft.kr:8500"
+
+		url = f"{self.danakka_url}/fishing/crawler/read/sunsang24/fishing_data/"
 		res = requests.get(url)
 		res.raise_for_status() # 문제시 프로그램 종료
 		res = res.json()
 
 		for res in res:
-			pk = res['pk']
-			business_name = res['fields']['display_business_name']
-			self.driver.get(f"https://www.google.com/search?q=선상24 {business_name}")
+			pk = res['id']
+			business_name = res['display_business_name']
+			site_url = res['site_url']
+			business_address = res['business_address']
+			if site_url is None:
+				self.driver.get(f"https://www.google.com/search?q={display_business_name} {business_address}")
 
-			if self.driver.find_elements(By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']"):
-				count = 1
-				while True:
-					try:
-						WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[src^='https://www.google.com/recaptcha/api2/anchor']")))
-						WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "span#recaptcha-anchor"))).click()
-						driver.switch_to.default_content()
-						WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"//iframe[@title='recaptcha challenge']")))
-						WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='solver-button']"))).click()
-					except:
-						pass
-					time.sleep(30)
-					if self.driver.find_elements(By.XPATH,"//div[@class='hdtb-mitem hdtb-msel']"):
+				if self.driver.find_elements(By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']"):
+					count = 1
+					while True:
+						try:
+							WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[src^='https://www.google.com/recaptcha/api2/anchor']")))
+							WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "span#recaptcha-anchor"))).click()
+							driver.switch_to.default_content()
+							WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"//iframe[@title='recaptcha challenge']")))
+							WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='solver-button']"))).click()
+						except:
+							pass
+						time.sleep(30)
+						if self.driver.find_elements(By.XPATH,"//div[@class='hdtb-mitem hdtb-msel']"):
+							break
+						elif count >=3:
+							return
+						count +=1
+
+				html = self.driver.page_source
+				soup = BeautifulSoup(html, 'html.parser')
+
+				links = soup.findAll('div',attrs={'class':'yuRUbf'})
+				for link in links:
+					link = link.find('a')['href']
+					link = link.split(".com")
+					link = link[0]+".com/"
+
+					if not "www.sunsang24.com" in link and "sunsang24.com" in link:
+						data = {
+							"site_url" : link,
+							"pk" : pk,
+							
+						}
+
+						res = requests.post(self.danakka_url+'/fishing/crawler/update/site_url/', data=data)
+						res.raise_for_status() # 문제시 프로그램 종료
+						res = res.json()
 						break
-					elif count >=3:
-						return
-					count +=1
-
-			html = self.driver.page_source
-			soup = BeautifulSoup(html, 'html.parser')
-
-			links = soup.findAll('div',attrs={'class':'yuRUbf'})
-			for link in links:
-				link = link.find('a')['href']
-				link = link.split(".com")
-				link = link[0]+".com/"
-
-				if not "www.sunsang24.com" in link and "sunsang24.com" in link:
-					data = {
-						"site_url" : link,
-						"pk" : pk,
-						
-					}
-
-					res = requests.post(url, data=data)
-					res.raise_for_status() # 문제시 프로그램 종료
-					res = res.json()
-					break
 			
 
 		self.driver.quit()
