02fc2c5e1153a9f0e303fbeef4c22df936e4c3df
newsoft111 <ydj0617@gmail.com>
2023-08-07 20:35:46 +0900

    cd

--- danakka-frontend/src/app/auth/mypage/ticket/page.tsx
+++ danakka-frontend/src/app/auth/mypage/ticket/page.tsx
@@ -5,7 +5,11 @@ import {
 	Heading, 
 	Text, 
 	Flex,
-	Link,
+	NumberInput,
+	NumberInputField ,
+	NumberInputStepper,
+	NumberIncrementStepper ,
+	NumberDecrementStepper ,
 	Box, 
 	Card, 
 	CardBody,
@@ -17,10 +21,13 @@ import {
 	ModalFooter,
 	ModalBody,
 	ModalCloseButton,
+	RadioGroup,
+	Stack,
+	Radio,
 } from "@chakra-ui/react";
 
 import MyPageNavBar from "../../../../component/Layout/Auth/MyPage/NavBar";
-
+import * as PortOne from '@portone/browser-sdk/v2';
 
 const MyPageTicket = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -32,6 +39,30 @@ const MyPageTicket = () => {
 	const handleCloseModal = () => {
 		setIsModalOpen(false);
 	};
+
+
+	const [count, setCount] = useState(1);
+	const [paymentMethod, setPaymentMethod] = useState("card");
+
+	const handleInputChange = (valueAsString: string, valueAsNumber: number) => {
+		if (!valueAsString) {
+			setCount(valueAsNumber);
+		}
+	};
+
+	const requestPayment = () => {
+		PortOne.requestPayment({
+			storeId: 'store-0d0fe347-57a0-4059-bbac-4dadd79b9739', // 가맹점 storeId로 변경해주세요.
+			paymentId: 'paymentId_{now()}',
+			orderName: '나이키 와플 트레이너 2 SD',
+			totalAmount: 1000,
+			currency: 'CURRENCY_KRW',
+			channelKey: 'channel-key-2c6d1ade-9c11-4ac2-8fb1-a3c85e72a90d', // 콘솔 결제 연동 화면에서 채널 연동 시 생성된 채널 키를 입력해주세요.
+			payMethod: "CARD"
+		});
+	  }
+
+	
 
 	return (
 		<>
@@ -70,15 +101,49 @@ const MyPageTicket = () => {
 					<ModalHeader>다낚아 티켓충전</ModalHeader>
 					<ModalCloseButton />
 					<ModalBody>
-						{/* Your content for the modal */}
-						<Text>Modal content goes here...</Text>
+
+						<Flex alignItems="center">
+							<Text mr={3} minW="100px">개수</Text>
+
+							<NumberInput defaultValue={count} min={1} onChange={handleInputChange}>
+								<NumberInputField />
+								<NumberInputStepper>
+									<NumberIncrementStepper />
+									<NumberDecrementStepper />
+								</NumberInputStepper>
+							</NumberInput>
+						</Flex>
+
+						<Flex alignItems="center" mt={5}>
+							<Text mr={3} minW="100px">금액</Text>
+
+							<Box>
+								<Text>티켓 {count}개 / {count*100} 원</Text>
+							</Box>
+						</Flex>
+
+						<Flex alignItems="center" mt={5}>
+							<Text mr={3} minW="100px">결제수단</Text>
+							
+							<RadioGroup onChange={setPaymentMethod} value={paymentMethod}>
+								<Stack direction='row'>
+									<Radio colorScheme='teal' value='card'>카드결제</Radio>
+									<Radio colorScheme='teal' value='kakao'>카카오페이</Radio>
+								</Stack>
+							</RadioGroup>
+						</Flex>
 					</ModalBody>
 
 					<ModalFooter>
-					<Button mr={3} onClick={handleCloseModal}>
-						닫기
-					</Button>
-					<Button colorScheme="teal" >결제</Button>
+						<Button mr={3} onClick={handleCloseModal}>
+							닫기
+						</Button>
+						<Button
+							colorScheme='teal'
+							onClick={requestPayment}
+						>
+							결제
+						</Button>
 					</ModalFooter>
 				</ModalContent>
 			</Modal>
