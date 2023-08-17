617e3c58919c178fcddd1de45a2ced12f0696be7
newsoft111 <ydj0617@gmail.com>
2023-08-17 23:05:55 +0900

    p

--- danakka-frontend/src/app/providers.tsx
+++ danakka-frontend/src/app/providers.tsx
@@ -13,13 +13,14 @@ import CenterSnipper from "../component/Common/CenterSnipper";
 import '@fontsource/noto-sans-kr/400.css'
 import '@fontsource/noto-sans-kr/700.css'
 import AuthManager from '../util/Authentication'; 
+import { usePathname } from 'next/navigation';
 
 export function Providers({ 
     children 
 }: { 
   	children: React.ReactNode 
 }) {
-
+	const pathname = usePathname();
 	const [isLoading, setIsLoading] = useState(true);
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -28,29 +29,21 @@ export function Providers({
 	}, []);
 
 	useEffect(() => {
-		const verifyUser = async () => {
-			try {
-				const user = await AuthManager.verifyToken();
-				if (user) {
-					setIsLoggedIn(true);
-				} else {
-					setIsLoggedIn(false);
-				}
-			} catch (error) {
+		verifyUser();
+	}, [pathname]);
+
+	const verifyUser = async () => {
+		try {
+			const user = await AuthManager.verifyToken();
+			if (user) {
+				setIsLoggedIn(true);
+			} else {
 				setIsLoggedIn(false);
 			}
-		};
-
-		verifyUser();
-	
-		const intervalId = setInterval(() => {
-            verifyUser();
-        }, 5000);
-
-        return () => {
-            clearInterval(intervalId); // 컴포넌트 언마운트 시 인터벌 해제
-        };
-	}, []);
+		} catch (error) {
+			setIsLoggedIn(false);
+		}
+	};
 
 
 	const [queryClient] = useState(() => new QueryClient())
